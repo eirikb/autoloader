9182e2de6e487b60cc62a1e733bc74dba830c9e5
Eirik Brandtzæg <eirikb@eirikb.no>
2011-05-21 17:46:35 +0200

    plugins.js module for hacking in plugins, like a hack

---
+++ plugins.js
@@ -0,0 +1,24 @@
+var fs = require('fs'),
+plugins = {},
+watchDir= function(dir) {
+	fs.readdir(dir, function(err, files) {
+		files.forEach(function(file) {
+			file = dir + file;
+			plugins[file] = require(file);
+
+			fs.watchFile(file, function(current, previous) {
+				if ( + current.mtime !== + previous.mtime) {
+					plugins[file].destruct && plugins[file].destruct();
+					delete require.cache[require.resolve(file)];
+					plugins[file] = require(file);
+				}
+			});
+		});
+	});
+};
+
+exports.watch = function(dir) {
+    watch(dir);
+    fs.watchFile(dir, watch(dir));
+};
+
