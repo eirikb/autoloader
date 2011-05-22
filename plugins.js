35c41d5fe1579dd907179f295d71e129a7cb0b3f
Eirik Brandtzæg <eirikb@eirikb.no>
2011-05-22 12:50:15 +0200

    try-catching so bot wont crash when plugins are foobar

--- plugins.js
+++ plugins.js
@@ -1,16 +1,24 @@
 var fs = require('fs'),
 plugins = {},
+req = function(file) {
+	try {
+		return require(file);
+	} catch(e) {
+		console.log('Error', file, e);
+	}
+	return null;
+},
 watchDir = function(dir) {
 	fs.readdir(dir, function(err, files) {
 		files.forEach(function(file) {
 			file = dir + file;
-			plugins[file] = require(file);
+			plugins[file] = req(file);
 
 			fs.watchFile(file, function(current, previous) {
 				if ( + current.mtime !== + previous.mtime) {
-					plugins[file].destruct && plugins[file].destruct();
+					plugins[file] && plugins[file].destruct && plugins[file].destruct();
 					delete require.cache[require.resolve(file)];
-					plugins[file] = require(file);
+					plugins[file] = req(file);
 				}
 			});
 		});
