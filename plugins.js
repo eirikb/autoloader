05c46c97136180288e048444c9db13a207c1cf53
Eirik Brandtzæg <eirikb@eirikb.no>
2011-08-11 14:05:45 +0200

    New setup!

--- plugins.js
+++ plugins.js
@@ -1,38 +1,42 @@
 var fs = require('fs'),
 path = require('path'),
-bot = require('bot.js'),
+winston = require('winston'),
+bot = require('./bot.js'),
 pluginFullPath = path.join('/', path.join(path.dirname(__filename), 'plugins/')),
 plugins = {},
 req = function(file) {
-    console.log('Require plugin %s', file);
+    var plugin = file.replace(/.*\//, '').green;
+    winston.info('Require plugin ' + plugin);
     try {
         return require(file);
     } catch(e) {
-        console.log('Error while loading plugin %s:', file, e);
+        winston.error('Error while loading plugin ' + plugin + ' (' + e.toString() + ')');
     }
     return null;
 };
 
 exports.watch = function() {
-    console.log('Watching %s for plugins', pluginFullPath);
+    winston.info('Watching ' + pluginFullPath.yellow + ' for plugins');
     fs.readdir(pluginFullPath, function(err, files) {
         files.forEach(function(file) {
+            if (file.match(/\.js$/)) {
 
-            file = pluginFullPath + file;
-            plugins[file] = req(file);
+                file = pluginFullPath + file;
+                plugins[file] = req(file);
 
-            fs.watchFile(file, function(current, previous) {
-                if ( + current.mtime !== + previous.mtime) {
-                    if (plugins[file]) {
-                        bot.removeListeners(plugins[file]);
-                        if (plugins[file].destruct) {
-                            plugins[file].destruct();
+                fs.watchFile(file, function(current, previous) {
+                    if ( + current.mtime !== + previous.mtime) {
+                        if (plugins[file]) {
+                            bot.removeListeners(file);
+                            if (plugins[file].destruct) {
+                                plugins[file].destruct();
+                            }
                         }
+                        delete require.cache[require.resolve(file)];
+                        plugins[file] = req(file);
                     }
-                    delete require.cache[require.resolve(file)];
-                    plugins[file] = req(file);
-                }
-            });
+                });
+            }
         });
     });
 };
